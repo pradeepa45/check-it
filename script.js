7d85f3be26c9f018b16df1a1214634ba1f767f35
Pradeepa Seelam <39674626+pradeepa45@users.noreply.github.com>
2021-04-15 21:02:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ function displayFunc(){
     var fileInput = document.getElementById("input-file");
     var l = fileInput.value;
     document.getElementById("path-display").value = l;
+    localStorage.clear();
 }
 
 function readFile(input){
@@ -14,55 +15,52 @@ function readFile(input){
         document.getElementById('text-content').innerHTML = reader.result;
         wrongWords=[];
         enteredWords=reader.result.split(/\W+/);
-        // console.log(enteredWords);
         let url1 = `https://api.textgears.com/spelling?key=zmeWUWHyKUYTqHFy&text=${inputText}&language=en-GB`;
         let req1 = new XMLHttpRequest();
         req1.open("GET",url1);
         req1.send();
+        // valueArray=[];
         req1.onload = ()=>{
             let parsedResponse1 = JSON.parse(req1.response);
-            console.log(parsedResponse1.response.errors.length);
-            for(var i =0; i< parsedResponse1.response.errors.length; i++){
-                wrongWords.push(parsedResponse1.response.errors[i].bad);
-                setContextMenu(parsedResponse1.response.errors[i]);
-                localStorage.setItem(parsedResponse1.response.errors[i].bad,parsedResponse1.response.errors[i].better);
-                // console.log(parsedResponse1.response.errors[i].bad);
-            }
-            // console.log(wrongWords);
-            pTag = document.createElement("p");
-            for(var x of enteredWords){
-                // console.log(x);
-                id=0;
-                var span = document.createElement("span");
-                var spanContent = document.createTextNode(x);
-                if(wrongWords.includes(x)){
-                    span.append(spanContent);
-                    pTag.append(span);
-                    span.classList.add("wrong-red");
-                    id++;
-                    span.setAttribute("id",id);
-                    // span.setAttribute("class","wrong-word");
-                    span.setAttribute("value",x);
+                for(var i =0; i< parsedResponse1.response.errors.length; i++){ 
+                    valueArray=[];
+                    wrongWords.push(parsedResponse1.response.errors[i].bad);
+                    valueArray.push(parsedResponse1.response.errors[i].description.en);
+                    valueArray.push(parsedResponse1.response.errors[i].better);
+                    localStorage.setItem(parsedResponse1.response.errors[i].bad,valueArray);
                 }
-                else{
-                    var spanContent = document.createTextNode(" "+x+" ");
-                    pTag.append(spanContent);
+                pTag = document.createElement("p");
+                for(var x of enteredWords){
+                    id="this-one";
+                    if(wrongWords.includes(x)){
+                        var span = document.createElement("span");
+                        var spanContent = document.createTextNode(" "+x+" ");
+                        span.append(spanContent);
+                        pTag.append(span);
+                        span.classList.add("wrong-red");
+                        // id++;
+                        span.setAttribute("id",id);
+                        span.setAttribute("value",x);
+                    }
+                    else{
+                        var spanContent = document.createTextNode(" "+x+" ");
+                        pTag.append(spanContent);
+                    }
+                    document.getElementById("text-content").innerHTML="";
+                    document.getElementById("text-content").append(pTag);
                 }
-            }
-            // console.log(pTag);
-            document.getElementById("text-content").innerHTML="";
-            document.getElementById("text-content").append(pTag);
-        }        
+                var invalidSpans = document.querySelectorAll('.wrong-red');
+                console.log(invalidSpans)
+                console.log(invalidSpans.length);
+                for( let i=0;i<invalidSpans.length;i++){
+                    $(invalidSpans[i]).contextmenu((event)=>{
+                        console.log(invalidSpans[i].innerHTML);
+                    })
+                }
+        } 
         reader.onerror = ()=>{
             console.log(reader.error);
         };
-    }
-    
+    }    
 }
 
-function setContextMenu(array){
-    // console.log(array);
-    console.log(array.description.en);
-    console.log(array.better);
-    // document.getElementById("")
-}
